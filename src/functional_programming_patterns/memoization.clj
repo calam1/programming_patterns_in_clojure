4b15c2321e412491a0e50ba9c3da320f4353c966
Christopher Lam <chrislam67@gmail.com>
2016-01-03 13:59:41 -0600

    memoization

---
+++ src/functional_programming_patterns/memoization.clj
@@ -0,0 +1,31 @@
+(ns functional-programming-patterns.memoization)
+
+(defn expensive-lookup
+  [id]
+  (Thread/sleep 1000)
+  (println (str "Lookup for " id))
+  ({42 "foo" 12 "bar" 1 "baz"} id))
+
+(def memoized-expensive-lookup
+  (memoize expensive-lookup))
+
+
+(def slow-fib
+  (fn [n]
+    (cond
+     (<= n 0) 0
+     (< n 2) 1
+     :else (+ (slow-fib (- n 1)) (slow-fib (- n 2))))))
+
+(def mem-fib
+  (memoize
+   (fn [n]
+     (cond
+      (<= n 0) 0
+      (< n 2) 1
+      :else (+ (mem-fib (- n 1)) (mem-fib (- n 2)))))))
+
+;; usage
+; (time (slow-fib 40))
+; (time (mem-fib 40))
+
