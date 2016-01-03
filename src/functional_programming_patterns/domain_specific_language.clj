ee5f24d178b13bfb5f9f4c913a5c6df6a6acf64f
Christopher Lam <chrislam67@gmail.com>
2016-01-03 15:56:39 -0600

    domain specific language - exercises not implemented - empty

---
+++ src/functional_programming_patterns/domain_specific_language.clj
@@ -0,0 +1,6 @@
+(ns functional-programming-patterns.domain-specific-language)
+
+; examples using it as a script language i.e. shell commands
+
+; clojure not good for scripting, startup too slow, so didn't bother
+; doing exercises
